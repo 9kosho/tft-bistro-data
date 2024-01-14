ba7159299676630b836032ec7a190b5f99603eb6
9kosho <9kosho@gmail.com>
2024-01-13 18:18:34 -0800

    Remove unnecessary code and update homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,40 +6,12 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home({ data }) {
-    const jsonString = JSON.stringify(data, null, 2);
+export default function Home() {
+    // const jsonString = JSON.stringify(data, null, 2);
 
     return (
         <main>
-            <pre>{jsonString}</pre>
+            <div>hello world</div>
         </main>
     );
-}
-
-export async function getStaticProps() {
-    const directoryPath = path.join(process.cwd(), "public", "json");
-    const filenames = fs.readdirSync(directoryPath);
-
-    let combinedData = [];
-
-    filenames
-        .filter((filename) => /\.json$/.test(filename))
-        .forEach((filename) => {
-            const filePath = path.join(directoryPath, filename);
-            const fileContents = fs.readFileSync(filePath, "utf8");
-            const jsonData = JSON.parse(fileContents);
-
-            // Merge the data arrays from each file into combinedData
-            if (Array.isArray(jsonData)) {
-                combinedData = combinedData.concat(jsonData);
-            } else {
-                combinedData.push(jsonData);
-            }
-        });
-
-    return {
-        props: {
-            data: combinedData,
-        },
-    };
 }
