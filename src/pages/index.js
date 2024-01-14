00e7daf791a1b621493ea6cc46bdcddb62867d84
9kosho <9kosho@gmail.com>
2024-01-13 18:20:39 -0800

    Remove unused imports and commented code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,10 @@
 import React from "react";
-import path from "path";
-import fs from "fs";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-    // const jsonString = JSON.stringify(data, null, 2);
-
     return (
         <main>
             <div>hello world</div>
